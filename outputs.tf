89d5c59e636fbf4869b7a031fac2db6f96cb897e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "data_factory_flowlet_data_flows" {
-  description = "All data_factory_flowlet_data_flow resources"
-  value       = azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows
-}
 output "data_factory_flowlet_data_flows_annotations" {
-  description = "List of annotations values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.annotations]
+  description = "Map of annotations values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.annotations }
 }
 output "data_factory_flowlet_data_flows_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.data_factory_id }
 }
 output "data_factory_flowlet_data_flows_description" {
-  description = "List of description values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.description]
+  description = "Map of description values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.description }
 }
 output "data_factory_flowlet_data_flows_folder" {
-  description = "List of folder values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.folder]
+  description = "Map of folder values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.folder }
 }
 output "data_factory_flowlet_data_flows_name" {
-  description = "List of name values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.name]
+  description = "Map of name values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.name }
 }
 output "data_factory_flowlet_data_flows_script" {
-  description = "List of script values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.script]
+  description = "Map of script values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.script }
 }
 output "data_factory_flowlet_data_flows_script_lines" {
-  description = "List of script_lines values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.script_lines]
+  description = "Map of script_lines values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.script_lines }
 }
 output "data_factory_flowlet_data_flows_sink" {
-  description = "List of sink values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.sink]
+  description = "Map of sink values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.sink }
 }
 output "data_factory_flowlet_data_flows_source" {
-  description = "List of source values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.source]
+  description = "Map of source values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.source }
 }
 output "data_factory_flowlet_data_flows_transformation" {
-  description = "List of transformation values across all data_factory_flowlet_data_flows"
-  value       = [for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : v.transformation]
+  description = "Map of transformation values across all data_factory_flowlet_data_flows, keyed the same as var.data_factory_flowlet_data_flows"
+  value       = { for k, v in azurerm_data_factory_flowlet_data_flow.data_factory_flowlet_data_flows : k => v.transformation }
 }
 
